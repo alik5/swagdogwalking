8ecef3cbd19d54c449899510f8b001423f969184
Ali FitzGerald <alikfitz@gmail.com>
2015-02-24 12:30:42 -0500

    show/hide javascript

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('#event_booked').change(function(){
+        if(this.checked)
+            $('#autoUpdate').hide();
+        else
+            $('#autoUpdate').fadeIn('slow');
+
+    });
+});
